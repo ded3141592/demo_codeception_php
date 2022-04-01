4f46cfa05c62bc6259673d20cbb7c86403157e8e
ded <ded3141592@gmail.com>
2022-04-01 20:08:12 +0300

    refactor after codereview;

--- tests/acceptance/DemoCest.php
+++ tests/acceptance/DemoCest.php
@@ -4,13 +4,20 @@ use pages\VideoPage;
 
 class DemoCest
 {
-
     public
     function videoPreviewTest(AcceptanceTester $I)
     {
-        $I->amOnPage(VideoPage::$url);
-        $searchCountResult = VideoPage::searchVideo($I,"ураган");
-        VideoPage::validateVideoTrailer($I, VideoPage::getVideoLocator(rand(1, $searchCountResult)));
+        $videoPage = new VideoPage($I);
+
+        $I->amOnPage(VideoPage::URL);
+        $searchResults = $videoPage->searchVideos("ураган");
+        $I->assertGreaterThan(0, count($searchResults));
+        $videoLocator = $searchResults[rand(0, count($searchResults) - 1)];
+        $screenName = str_replace(' ', '', microtime());
+        $I->makeElementScreenshot($videoLocator, $screenName);
+        $I->moveMouseOver($videoLocator);
+        $trailerIsPlaying = $I->waitForElementDisplayingChanged($screenName, $videoLocator);
+        $I->assertTrue($trailerIsPlaying, "Trailer doesn't play");
     }
 
 }
